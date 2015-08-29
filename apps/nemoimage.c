9999fce38dcd43e2ef05598c8a6630ba3926e645
root <haruband@gmail.com>
2015-08-29 09:22:28 +0000

    Add image viewer

--- apps/nemoimage.c
+++ apps/nemoimage.c
@@ -10,49 +10,22 @@
 #include <nemotool.h>
 #include <nemocanvas.h>
 #include <nemoegl.h>
-#include <pdfhelper.h>
 #include <pixmanhelper.h>
 #include <talehelper.h>
 #include <nemomisc.h>
 
-struct pdfcontext {
+struct imagecontext {
 	struct nemotool *tool;
 	struct eglcanvas *canvas;
 
 	struct nemotale *tale;
 	struct talenode *node;
-
-	struct nemopdf *pdf;
-
-	int is_fullscreen;
 };
 
-static uint32_t nemopdf_get_touch_location(int32_t sx, int32_t sy, int32_t sw, int32_t sh, int32_t x, int32_t y)
+static void nemoimage_dispatch_tale_event(struct nemotale *tale, struct talenode *node, uint32_t type, struct taleevent *event)
 {
-	int32_t w = sw - sx;
-	int32_t h = sh - sy;
-	int32_t px = x - sx;
-	int32_t py = y - sy;
-	uint32_t type = 0x0;
-
-	if (px < w / 2)
-		type |= (1 << 0);
-	else
-		type |= (1 << 1);
-
-	if (py < h / 2)
-		type |= (1 << 2);
-	else
-		type |= (1 << 3);
-
-	return type;
-}
-
-static void nemopdf_dispatch_tale_event(struct nemotale *tale, struct talenode *node, uint32_t type, struct taleevent *event)
-{
-	struct pdfcontext *context = (struct pdfcontext *)nemotale_get_userdata(tale);
+	struct imagecontext *context = (struct imagecontext *)nemotale_get_userdata(tale);
 	struct nemocanvas *canvas = NTEGL_CANVAS(context->canvas);
-	struct nemopdf *pdf = context->pdf;
 	uint32_t id = nemotale_node_get_id(node);
 
 	if (id == 1) {
@@ -60,16 +33,8 @@ static void nemopdf_dispatch_tale_event(struct nemotale *tale, struct talenode *
 			nemotale_event_update_node_taps(tale, node, event, type);
 
 			if (event->tapcount == 1) {
-				if (context->is_fullscreen == 0) {
-					nemocanvas_move(canvas, event->taps[0]->serial);
-				}
+				nemocanvas_move(canvas, event->taps[0]->serial);
 			} else if (event->tapcount == 2) {
-				if (context->is_fullscreen != 0) {
-					nemocanvas_unset_fullscreen(canvas);
-
-					context->is_fullscreen = 0;
-				}
-
 				nemocanvas_pick(canvas,
 						event->taps[0]->serial,
 						event->taps[1]->serial,
@@ -79,47 +44,17 @@ static void nemopdf_dispatch_tale_event(struct nemotale *tale, struct talenode *
 			nemotale_event_update_node_taps(tale, node, event, type);
 
 			if (event->tapcount == 1) {
-				uint32_t location;
-
-				location = nemopdf_get_touch_location(0, 0,
-						nemotale_node_get_width(node),
-						nemotale_node_get_height(node),
-						(int32_t)event->x, (int32_t)event->y);
-
-				if (location & (1 << 0)) {
-					if (nemopdf_get_page_index(pdf) - 1 >= 0) {
-						nemopdf_render_page(pdf,
-								nemopdf_get_page_index(pdf) - 1,
-								nemotale_node_get_pixman(node));
-						nemotale_node_damage_all(node);
-						nemotale_composite_egl(tale, NULL);
-					}
-				} else if (location & (1 << 1)) {
-					if (nemopdf_get_page_index(pdf) + 1 < nemopdf_get_page_count(pdf)) {
-						nemopdf_render_page(pdf,
-								nemopdf_get_page_index(pdf) + 1,
-								nemotale_node_get_pixman(node));
-						nemotale_node_damage_all(node);
-						nemotale_composite_egl(tale, NULL);
-					}
-				}
 			} else if (event->tapcount == 2) {
 			} else if (event->tapcount == 3) {
-				if (context->is_fullscreen == 0) {
-					nemocanvas_set_fullscreen(canvas);
-
-					context->is_fullscreen = 1;
-				}
 			}
 		}
 	}
 }
 
-static void nemopdf_dispatch_canvas_resize(struct nemocanvas *canvas, int32_t width, int32_t height)
+static void nemoimage_dispatch_canvas_resize(struct nemocanvas *canvas, int32_t width, int32_t height)
 {
 	struct nemotale *tale = (struct nemotale *)nemocanvas_get_userdata(canvas);
-	struct pdfcontext *context = (struct pdfcontext *)nemotale_get_userdata(tale);
-	struct nemopdf *pdf = context->pdf;
+	struct imagecontext *context = (struct imagecontext *)nemotale_get_userdata(tale);
 	struct talenode *node = context->node;
 
 	if (width == 0 || height == 0)
@@ -127,16 +62,6 @@ static void nemopdf_dispatch_canvas_resize(struct nemocanvas *canvas, int32_t wi
 
 	if (width < 200 || height < 200)
 		nemotool_exit(context->tool);
-
-	nemotool_resize_egl_canvas(context->canvas, width, height);
-	nemotale_resize(tale, width, height);
-	nemotale_node_resize_pixman(node, width, height);
-
-	nemopdf_render_page(pdf,
-			nemopdf_get_page_index(pdf),
-			nemotale_node_get_pixman(node));
-	nemotale_node_damage_all(node);
-	nemotale_composite_egl(tale, NULL);
 }
 
 int main(int argc, char *argv[])
@@ -146,13 +71,13 @@ int main(int argc, char *argv[])
 		{ 0 }
 	};
 
-	struct pdfcontext *context;
-	struct nemopdf *pdf;
+	struct imagecontext *context;
 	struct nemotool *tool;
 	struct eglcontext *egl;
 	struct eglcanvas *canvas;
 	struct nemotale *tale;
 	struct talenode *node;
+	pixman_image_t *piximg;
 	char *filepath = NULL;
 	int32_t width, height;
 	int opt;
@@ -174,16 +99,20 @@ int main(int argc, char *argv[])
 	if (filepath == NULL)
 		return 0;
 
-	context = (struct pdfcontext *)malloc(sizeof(struct pdfcontext));
+	context = (struct imagecontext *)malloc(sizeof(struct imagecontext));
 	if (context == NULL)
 		return -1;
 
-	context->pdf = pdf = nemopdf_create_doc(filepath);
-	if (pdf == NULL)
-		goto out1;
+	piximg = pixman_load_png_file(filepath);
+	if (piximg == NULL) {
+		piximg = pixman_load_jpeg_file(filepath);
+		if (piximg == NULL) {
+			goto out1;
+		}
+	}
 
-	width = nemopdf_get_page_width(pdf);
-	height = nemopdf_get_page_height(pdf);
+	width = pixman_image_get_width(piximg);
+	height = pixman_image_get_height(piximg);
 
 	context->tool = tool = nemotool_create();
 	if (tool == NULL)
@@ -195,7 +124,7 @@ int main(int argc, char *argv[])
 	context->canvas = canvas = nemotool_create_egl_canvas(egl, width, height);
 	nemocanvas_set_nemosurface(NTEGL_CANVAS(canvas), NEMO_SHELL_SURFACE_TYPE_NORMAL);
 	nemocanvas_set_anchor(NTEGL_CANVAS(canvas), -0.5f, -0.5f);
-	nemocanvas_set_dispatch_resize(NTEGL_CANVAS(canvas), nemopdf_dispatch_canvas_resize);
+	nemocanvas_set_dispatch_resize(NTEGL_CANVAS(canvas), nemoimage_dispatch_canvas_resize);
 
 	context->tale = tale = nemotale_create_gl();
 	nemotale_set_backend(tale,
@@ -206,16 +135,20 @@ int main(int argc, char *argv[])
 				(EGLNativeWindowType)NTEGL_WINDOW(canvas)));
 	nemotale_resize(tale, width, height);
 
-	nemotale_attach_canvas(tale, NTEGL_CANVAS(canvas), nemopdf_dispatch_tale_event);
+	nemotale_attach_canvas(tale, NTEGL_CANVAS(canvas), nemoimage_dispatch_tale_event);
 	nemotale_set_userdata(tale, context);
 
 	context->node = node = nemotale_node_create_pixman(width, height);
 	nemotale_node_set_id(node, 1);
 	nemotale_attach_node(tale, node);
 
-	nemopdf_render_page(pdf,
-			0,
-			nemotale_node_get_pixman(node));
+	pixman_image_composite32(PIXMAN_OP_SRC,
+			piximg,
+			NULL,
+			nemotale_node_get_pixman(node),
+			0, 0, 0, 0, 0, 0,
+			width, height);
+
 	nemotale_composite_egl(tale, NULL);
 
 	nemotool_run(tool);
@@ -229,7 +162,7 @@ int main(int argc, char *argv[])
 	nemotool_destroy(tool);
 
 out2:
-	nemopdf_destroy_doc(pdf);
+	pixman_image_unref(piximg);
 
 out1:
 	free(context);
