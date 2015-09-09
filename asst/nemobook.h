298a7621bb3efd3fa00600ed0156abc10a7971c2
root <haruband@gmail.com>
2015-09-09 09:01:33 +0000

    Add type management to nemobook

--- asst/nemobook.h
+++ asst/nemobook.h
@@ -13,18 +13,25 @@ NEMO_BEGIN_EXTERN_C
 struct nemobook {
 	void **lists;
 	int nlists;
+
+	uint32_t *types;
 };
 
 static inline struct nemobook *nemobook_create(int count)
 {
 	struct nemobook *book;
 
-	book = (struct nemobook *)malloc(sizeof(struct nemobook) + sizeof(void *) * count);
+	book = (struct nemobook *)malloc(sizeof(struct nemobook));
 	if (book == NULL)
 		return NULL;
-	memset(book, 0, sizeof(struct nemobook) + sizeof(void *) * count);
+	memset(book, 0, sizeof(struct nemobook));
 
-	book->lists = (void **)((char *)book + sizeof(struct nemobook));
+	book->lists = (void **)malloc(sizeof(void *) * count);
+	memset(book->lists, 0, sizeof(void *) * count);
+
+	book->types = (uint32_t *)malloc(sizeof(uint32_t) * count);
+	memset(book->types, 0, sizeof(uint32_t) * count);
+
 	book->nlists = count;
 
 	return book;
@@ -32,12 +39,24 @@ static inline struct nemobook *nemobook_create(int count)
 
 static inline void nemobook_destroy(struct nemobook *book)
 {
+	free(book->types);
+	free(book->lists);
 	free(book);
 }
 
 static inline void nemobook_clear(struct nemobook *book)
 {
 	memset(book->lists, 0, sizeof(void *) * book->nlists);
+}
+
+static inline void nemobook_set_type(struct nemobook *book, int i, uint32_t type)
+{
+	book->types[i] = type;
+}
+
+static inline uint32_t nemobook_get_type(struct nemobook *book, int i)
+{
+	return book->types[i];
 }
 
 static inline void nemobook_set(struct nemobook *book, int i, void *v)
