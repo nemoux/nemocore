b0b4a81fb780fe910937d59033a23e23a0cd33c9
root <haruband@gmail.com>
2015-11-05 06:39:13 +0000

    Add calibration support to nemotouch

--- apps/nemotouch.cpp
+++ apps/nemotouch.cpp
@@ -43,6 +43,7 @@
 #include <nemoegl.h>
 #include <pixmanhelper.h>
 #include <talehelper.h>
+#include <nemolog.h>
 #include <nemomisc.h>
 
 struct touchcontext {
@@ -63,9 +64,12 @@ struct touchcontext {
 	double ytaps[128];
 	char staps[128][32];
 	int ntaps;
+
+	int is_calibration;
+	int corner;
 };
 
-static void nemotouch_render_one(struct touchcontext *touch, pixman_image_t *image)
+static void nemotouch_render_touchpoints(struct touchcontext *touch, pixman_image_t *image)
 {
 	int32_t width = pixman_image_get_width(image);
 	int32_t height = pixman_image_get_height(image);
@@ -105,6 +109,40 @@ static void nemotouch_render_one(struct touchcontext *touch, pixman_image_t *ima
 	}
 }
 
+static void nemotouch_render_corner(struct touchcontext *touch, pixman_image_t *image)
+{
+	int32_t width = pixman_image_get_width(image);
+	int32_t height = pixman_image_get_height(image);
+	int i;
+
+	SkBitmap bitmap;
+	bitmap.setInfo(
+			SkImageInfo::Make(width, height, kN32_SkColorType, kPremul_SkAlphaType));
+	bitmap.setPixels(
+			pixman_image_get_data(image));
+
+	SkBitmapDevice device(bitmap);
+	SkCanvas canvas(&device);
+
+	canvas.clear(SK_ColorBLACK);
+
+	SkPaint stroke;
+	stroke.setAntiAlias(true);
+	stroke.setStyle(SkPaint::kStroke_Style);
+	stroke.setColor(SK_ColorYELLOW);
+	stroke.setStrokeWidth(3.0f);
+
+	if (touch->corner == 0) {
+		canvas.drawCircle(width * 0.1f, height * 0.1f, 30.0f, stroke);
+	} else if (touch->corner == 1) {
+		canvas.drawCircle(width * 0.9f, height * 0.1f, 30.0f, stroke);
+	} else if (touch->corner == 2) {
+		canvas.drawCircle(width * 0.1f, height * 0.9f, 30.0f, stroke);
+	} else if (touch->corner == 3) {
+		canvas.drawCircle(width * 0.9f, height * 0.9f, 30.0f, stroke);
+	}
+}
+
 static void nemotouch_dispatch_canvas_frame(struct nemocanvas *canvas, uint64_t secs, uint32_t nsecs)
 {
 	struct nemotale *tale = (struct nemotale *)nemocanvas_get_userdata(canvas);
@@ -115,8 +153,13 @@ static void nemotouch_dispatch_canvas_frame(struct nemocanvas *canvas, uint64_t 
 		nemocanvas_feedback(canvas);
 	}
 
-	nemotouch_render_one(touch,
-			nemotale_node_get_pixman(node));
+	if (touch->is_calibration == 0) {
+		nemotouch_render_touchpoints(touch,
+				nemotale_node_get_pixman(node));
+	} else {
+		nemotouch_render_corner(touch,
+				nemotale_node_get_pixman(node));
+	}
 
 	nemotale_node_damage_all(node);
 
@@ -127,48 +170,78 @@ static void nemotouch_dispatch_tale_event(struct nemotale *tale, struct talenode
 {
 	struct touchcontext *touch = (struct touchcontext *)nemotale_get_userdata(tale);
 	uint32_t id = nemotale_node_get_id(node);
-	int i, ntaps = 0;
 
-	nemotale_event_update_taps(tale, event, type);
+	if (touch->is_calibration == 0) {
+		int i, ntaps = 0;
 
-	for (i = 0; i < event->tapcount; i++) {
-		if (type == NEMOTALE_TOUCH_UP_EVENT && event->device == event->taps[i]->device)
-			continue;
+		nemotale_event_update_taps(tale, event, type);
 
-		touch->xtaps[ntaps] = event->taps[i]->x;
-		touch->ytaps[ntaps] = event->taps[i]->y;
+		for (i = 0; i < event->tapcount; i++) {
+			if (type == NEMOTALE_TOUCH_UP_EVENT && event->device == event->taps[i]->device)
+				continue;
 
-		snprintf(touch->staps[ntaps], 32, "%u", event->taps[i]->device);
+			touch->xtaps[ntaps] = event->taps[i]->x;
+			touch->ytaps[ntaps] = event->taps[i]->y;
 
-		ntaps++;
+			snprintf(touch->staps[ntaps], 32, "%u", event->taps[i]->device);
+
+			nemolog_message("TOUCH", "[%d TAP] %f %f\n", i, touch->xtaps[ntaps], touch->ytaps[ntaps]);
+
+			ntaps++;
+		}
+
+		touch->ntaps = ntaps;
+
+		nemocanvas_dispatch_frame(touch->canvas);
+	} else {
+		if (nemotale_is_long_press(tale, event, type)) {
+			nemotale_tap_set_state(event->tap, NEMOTALE_TAP_USED_STATE);
+
+			nemolog_message("TOUCH", "[%d CORNER] %f %f\n", touch->corner, event->x, event->y);
+
+			if (++touch->corner >= 4) {
+				nemotool_exit(touch->tool);
+			}
+
+			nemocanvas_dispatch_frame(touch->canvas);
+		}
 	}
+}
 
-	touch->ntaps = ntaps;
+static void nemotouch_dispatch_tale_timer(struct nemotimer *timer, void *data)
+{
+	struct touchcontext *touch = (struct touchcontext *)data;
 
-	nemocanvas_dispatch_frame(touch->canvas);
+	nemotimer_set_timeout(timer, 500);
+
+	nemotale_push_timer_event(touch->tale, time_current_msecs());
 }
 
 int main(int argc, char *argv[])
 {
 	struct option options[] = {
-		{ "width",			required_argument,			NULL,		'w' },
-		{ "height",			required_argument,			NULL,		'h' },
-		{ "uri",				required_argument,			NULL,		'u' },
-		{ "background",	no_argument,						NULL,		'b' },
+		{ "width",				required_argument,			NULL,		'w' },
+		{ "height",				required_argument,			NULL,		'h' },
+		{ "uri",					required_argument,			NULL,		'u' },
+		{ "calibration",	no_argument,						NULL,		'c' },
 		{ 0 }
 	};
 	struct touchcontext *touch;
 	struct nemotool *tool;
+	struct nemotimer *timer;
 	struct eglcontext *egl;
 	struct eglcanvas *canvas;
 	struct nemotale *tale;
 	int32_t width = 1920;
 	int32_t height = 1080;
 	char *uri = NULL;
+	int is_calibration = 0;
 	int opt;
 	int i;
 
-	while (opt = getopt_long(argc, argv, "w:h:u:b", options, NULL)) {
+	nemolog_set_file(2);
+
+	while (opt = getopt_long(argc, argv, "w:h:u:c", options, NULL)) {
 		if (opt == -1)
 			break;
 
@@ -185,6 +258,10 @@ int main(int argc, char *argv[])
 				uri = strdup(optarg);
 				break;
 
+			case 'c':
+				is_calibration = 1;
+				break;
+
 			default:
 				break;
 		}
@@ -197,6 +274,9 @@ int main(int argc, char *argv[])
 
 	touch->width = width;
 	touch->height = height;
+
+	touch->is_calibration = is_calibration;
+	touch->corner = 0;
 
 	touch->tool = tool = nemotool_create();
 	if (tool == NULL)
@@ -233,9 +313,16 @@ int main(int argc, char *argv[])
 	nemotale_node_set_id(touch->node, 1);
 	nemotale_attach_node(tale, touch->node);
 
+	timer = nemotimer_create(tool);
+	nemotimer_set_callback(timer, nemotouch_dispatch_tale_timer);
+	nemotimer_set_timeout(timer, 500);
+	nemotimer_set_userdata(timer, touch);
+
 	nemocanvas_dispatch_frame(NTEGL_CANVAS(canvas));
 
 	nemotool_run(tool);
+
+	nemotimer_destroy(timer);
 
 	nemotale_destroy_gl(tale);
 
