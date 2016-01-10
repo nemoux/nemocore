44ef63690b18868e9d62cac737f4f1cf530f4c42
root <haruband@gmail.com>
2016-01-10 08:32:47 +0000

    Add cursor interface to nemoenvs

---
+++ envs/nemocursor.h
@@ -0,0 +1,21 @@
+#ifndef __NEMOSHELL_CURSOR_H__
+#define __NEMOSHELL_CURSOR_H__
+
+#include <nemoconfig.h>
+
+#ifdef __cplusplus
+NEMO_BEGIN_EXTERN_C
+#endif
+
+extern int nemocursor_make_circle(
+		void *pixels, int width, int height,
+		double x, double y, double r,
+		SkPaint::Style style,
+		SkColor color,
+		double blur);
+
+#ifdef __cplusplus
+NEMO_END_EXTERN_C
+#endif
+
+#endif
