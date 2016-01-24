15d2e9111fcdc99e328713b0179dfd71d53c2373
root <haruband@gmail.com>
2016-01-24 11:14:35 +0000

    Set rotate and scale minimum degree to 0.0f

--- shell/shell.c
+++ shell/shell.c
@@ -1191,8 +1191,8 @@ void nemoshell_load_gestures(struct nemoshell *shell)
 	shell->pick.max_scale = nemoitem_get_fattr_named(shell->configs, "//nemoshell/pick", "max_scale", 0.15f);
 	shell->pick.fullscreen_scale = nemoitem_get_fattr_named(shell->configs, "//nemoshell/pick", "fullscreen_scale", 1.5f);
 	shell->pick.resize_interval = nemoitem_get_fattr_named(shell->configs, "//nemoshell/pick", "resize_interval", 50.0f);
-	shell->pick.rotate_degree = nemoitem_get_fattr_named(shell->configs, "//nemoshell/pick", "rotate_degree", 15.0f);
-	shell->pick.scale_degree = nemoitem_get_fattr_named(shell->configs, "//nemoshell/pick", "scale_degree", 90.0f);
+	shell->pick.rotate_degree = nemoitem_get_fattr_named(shell->configs, "//nemoshell/pick", "rotate_degree", 0.0f);
+	shell->pick.scale_degree = nemoitem_get_fattr_named(shell->configs, "//nemoshell/pick", "scale_degree", 0.0f);
 	shell->pick.min_distance = nemoitem_get_fattr_named(shell->configs, "//nemoshell/pick", "min_distance", 50.0f);
 
 	shell->bin.min_width = nemoitem_get_iattr_named(shell->configs, "//nemoshell/bin", "min_width", 0);
