cdfc3f9d2db2e3ebb8dab46febf26e191376fae7
nemoux <haruband@gmail.com>
2016-04-29 23:19:42 -0400

    Should not show context menu for nemonavi

--- navi/naviclient.cpp
+++ navi/naviclient.cpp
@@ -31,6 +31,8 @@ NaviClient::~NaviClient()
 
 void NaviClient::OnBeforeContextMenu(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefContextMenuParams> params, CefRefPtr<CefMenuModel> model)
 {
+	model->Clear();
+
 	nemolog_message("NAVI", "[%s:%d]\n", __FUNCTION__, __LINE__);
 }
 
