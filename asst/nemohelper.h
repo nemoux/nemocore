448c31932b42be7b6c088141c955763acf8fa48b
nemoux <haruband@gmail.com>
2016-05-18 22:03:24 -0400

    Remove all backapps

--- asst/nemohelper.h
+++ asst/nemohelper.h
@@ -39,6 +39,29 @@ extern int os_set_nonblocking_mode(int fd);
 extern double cubicbezier_point(double t, double p0, double p1, double p2, double p3);
 extern double cubicbezier_length(double x0, double y0, double x1, double y1, double x2, double y2, double x3, double y3, int steps);
 
+#define	COLOR_TO_UINT32(b)	\
+	((uint32_t)b[3] << 24) | ((uint32_t)b[2] << 16) | ((uint32_t)b[1] << 8) | ((uint32_t)b[0] << 0)
+
+#define	COLOR_UINT32_A(u)	\
+	(uint8_t)((u >> 24) & 0xff)
+#define	COLOR_UINT32_R(u)	\
+	(uint8_t)((u >> 16) & 0xff)
+#define	COLOR_UINT32_G(u)	\
+	(uint8_t)((u >> 8) & 0xff)
+#define	COLOR_UINT32_B(u)	\
+	(uint8_t)((u >> 0) & 0xff)
+
+#define	COLOR_DOUBLE_A(u)	\
+	(double)COLOR_UINT32_A(u) / (double)255
+#define	COLOR_DOUBLE_R(u)	\
+	(double)COLOR_UINT32_R(u) / (double)255
+#define	COLOR_DOUBLE_G(u)	\
+	(double)COLOR_UINT32_G(u) / (double)255
+#define	COLOR_DOUBLE_B(u)	\
+	(double)COLOR_UINT32_B(u) / (double)255
+
+extern uint32_t color_parse(const char *value);
+
 #ifdef __cplusplus
 NEMO_END_EXTERN_C
 #endif
