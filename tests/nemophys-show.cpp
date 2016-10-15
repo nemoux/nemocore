1807430cd36bcdb640e7a1d4bf4b2d091aa1bb43
root <haruband@gmail.com>
2016-10-15 07:21:16 +0000

    nemophys-show: Add simple gravity simulation using bullet

--- tests/nemophys-show.cpp
+++ tests/nemophys-show.cpp
@@ -7,6 +7,15 @@
 
 #include <getopt.h>
 
+#include <BulletCollision/CollisionShapes/btBox2dShape.h>
+#include <BulletCollision/CollisionShapes/btConvex2dShape.h>
+#include <BulletCollision/CollisionDispatch/btEmptyCollisionAlgorithm.h>
+#include <BulletCollision/CollisionDispatch/btBox2dBox2dCollisionAlgorithm.h>
+#include <BulletCollision/CollisionDispatch/btConvex2dConvex2dAlgorithm.h>
+#include <BulletCollision/NarrowPhaseCollision/btMinkowskiPenetrationDepthSolver.h>
+
+#include <btBulletDynamicsCommon.h>
+
 #include <nemoshow.h>
 #include <showhelper.h>
 #include <skiahelper.hpp>
@@ -16,15 +25,33 @@
 struct physcontext {
 	struct nemotool *tool;
 
+	int width, height;
+
 	struct nemoshow *show;
 	struct showone *scene;
 	struct showone *back;
 
 	struct showone *canvas;
+
+	btBroadphaseInterface *broadphase;
+	btCollisionDispatcher *dispatcher;
+	btConstraintSolver *solver;
+	btDefaultCollisionConfiguration *configuration;
+
+	btConvex2dConvex2dAlgorithm::CreateFunc *convex2dalgo;
+	btBox2dBox2dCollisionAlgorithm::CreateFunc *box2dalgo;
+	btVoronoiSimplexSolver *simplexsolver;
+	btMinkowskiPenetrationDepthSolver *pdsolver;
+
+	btDiscreteDynamicsWorld *dynamicsworld;
+
+	btAlignedObjectArray<btRigidBody *> circles;
 };
 
 static void nemophys_dispatch_canvas_redraw(struct nemoshow *show, struct showone *one)
 {
+	struct physcontext *context = (struct physcontext *)nemoshow_get_userdata(show);
+
 	SkBitmap bitmap;
 	bitmap.setInfo(
 			SkImageInfo::Make(
@@ -38,16 +65,38 @@ static void nemophys_dispatch_canvas_redraw(struct nemoshow *show, struct showon
 	SkCanvas canvas(&device);
 	canvas.clear(SK_ColorTRANSPARENT);
 
+	sk_sp<SkMaskFilter> filter = SkBlurMaskFilter::Make(
+			kSolid_SkBlurStyle,
+			SkBlurMaskFilter::ConvertRadiusToSigma(7.0f),
+			SkBlurMaskFilter::kIgnoreTransform_BlurFlag);
+
 	SkPaint paint;
 	paint.setStyle(SkPaint::kStrokeAndFill_Style);
 	paint.setStrokeWidth(3.0f);
 	paint.setColor(SK_ColorYELLOW);
+	paint.setMaskFilter(filter);
 
-	SkRect rect = SkRect::MakeXYWH(50, 50, 250, 250);
+	context->dynamicsworld->stepSimulation(1.0f / 60.0f, 0);
 
-	canvas.drawRect(rect, paint);
+	for (int i = 0; i < context->circles.size(); i++) {
+		btRigidBody *body = context->circles[i];
+
+		if (body != NULL && body->getMotionState()) {
+			btTransform transform;
+
+			body->getMotionState()->getWorldTransform(transform);
+
+			canvas.drawCircle(
+					float(transform.getOrigin().getX()),
+					float(transform.getOrigin().getY()),
+					50.0f,
+					paint);
+		}
+	}
 
 	nemoshow_canvas_unmap(one);
+
+	nemoshow_one_dirty(one, NEMOSHOW_REDRAW_DIRTY);
 }
 
 static void nemophys_dispatch_canvas_event(struct nemoshow *show, struct showone *canvas, struct showevent *event)
@@ -56,9 +105,29 @@ static void nemophys_dispatch_canvas_event(struct nemoshow *show, struct showone
 
 	nemoshow_event_update_taps(show, canvas, event);
 
+	if (nemoshow_event_is_touch_down(show, event)) {
+		btConvexShape *cicle = new btCylinderShapeZ(btVector3(btScalar(10.0f), btScalar(10.0f), btScalar(0.04f)));
+		btConvexShape *shape = new btConvex2dShape(cicle);
+
+		btTransform transform;
+		transform.setIdentity();
+		transform.setOrigin(btVector3(nemoshow_event_get_x(event), nemoshow_event_get_y(event), 0.0f));
+
+		btScalar mass(1.0f);
+		btVector3 linertia(0, 0, 0);
+
+		btDefaultMotionState *motionstate = new btDefaultMotionState(transform);
+		btRigidBody::btRigidBodyConstructionInfo bodyinfo(mass, motionstate, shape, linertia);
+		btRigidBody *body = new btRigidBody(bodyinfo);
+
+		context->dynamicsworld->addRigidBody(body);
+
+		context->circles.push_back(body);
+	}
+
 	if (nemoshow_event_is_touch_down(show, event) || nemoshow_event_is_touch_up(show, event)) {
 		if (nemoshow_event_is_more_taps(show, event, 3)) {
-			nemoshow_view_pick_distant(show, event, NEMOSHOW_VIEW_PICK_ALL_TYPE);
+			nemoshow_view_pick_distant(show, event, NEMOSHOW_VIEW_PICK_ROTATE_TYPE | NEMOSHOW_VIEW_PICK_TRANSLATE_TYPE);
 
 			nemoshow_event_set_cancel(event);
 
@@ -76,6 +145,66 @@ static void nemophys_dispatch_show_resize(struct nemoshow *show, int32_t width, 
 	nemoshow_view_redraw(context->show);
 }
 
+static int nemophys_prepare_bullet(struct physcontext *context)
+{
+	context->configuration = new btDefaultCollisionConfiguration();
+	context->dispatcher = new btCollisionDispatcher(context->configuration);
+	context->simplexsolver = new btVoronoiSimplexSolver();
+	context->pdsolver = new btMinkowskiPenetrationDepthSolver();
+
+	context->convex2dalgo = new btConvex2dConvex2dAlgorithm::CreateFunc(context->simplexsolver, context->pdsolver);
+	context->box2dalgo = new btBox2dBox2dCollisionAlgorithm::CreateFunc();
+
+	context->dispatcher->registerCollisionCreateFunc(CONVEX_2D_SHAPE_PROXYTYPE, CONVEX_2D_SHAPE_PROXYTYPE, context->convex2dalgo);
+	context->dispatcher->registerCollisionCreateFunc(BOX_2D_SHAPE_PROXYTYPE, CONVEX_2D_SHAPE_PROXYTYPE, context->convex2dalgo);
+	context->dispatcher->registerCollisionCreateFunc(CONVEX_2D_SHAPE_PROXYTYPE, BOX_2D_SHAPE_PROXYTYPE, context->convex2dalgo);
+	context->dispatcher->registerCollisionCreateFunc(BOX_2D_SHAPE_PROXYTYPE, BOX_2D_SHAPE_PROXYTYPE, context->box2dalgo);
+
+	context->broadphase = new btDbvtBroadphase();
+	context->solver = new btSequentialImpulseConstraintSolver;
+
+	context->dynamicsworld = new btDiscreteDynamicsWorld(context->dispatcher, context->broadphase, context->solver, context->configuration);
+	context->dynamicsworld->setGravity(btVector3(0, 100, 0));
+
+	btCollisionShape *ground = new btBoxShape(btVector3(btScalar(context->width), btScalar(context->height), btScalar(100.0f)));
+
+	btTransform transform;
+	transform.setIdentity();
+	transform.setOrigin(btVector3(0, 0, 0));
+
+	btScalar mass(0.0f);
+	btVector3 linertia(0, 0, 0);
+
+	if (mass != 0.0f)
+		ground->calculateLocalInertia(mass, linertia);
+
+	btDefaultMotionState *motionstate = new btDefaultMotionState(transform);
+	btRigidBody::btRigidBodyConstructionInfo bodyinfo(mass, motionstate, ground, linertia);
+	btRigidBody *body = new btRigidBody(bodyinfo);
+
+	context->dynamicsworld->addRigidBody(body);
+
+	return 0;
+}
+
+static void nemophys_finish_bullet(struct physcontext *context)
+{
+	delete context->dynamicsworld;
+
+	delete context->solver;
+
+	delete context->broadphase;
+	delete context->dispatcher;
+
+	delete context->configuration;
+
+	delete context->convex2dalgo;
+	delete context->box2dalgo;
+
+	delete context->simplexsolver;
+	delete context->pdsolver;
+}
+
 int main(int argc, char *argv[])
 {
 	struct option options[] = {
@@ -88,6 +217,7 @@ int main(int argc, char *argv[])
 	struct showone *scene;
 	struct showone *canvas;
 	struct showone *one;
+	struct showtransition *trans;
 	int width = 800;
 	int height = 800;
 	int opt;
@@ -108,6 +238,9 @@ int main(int argc, char *argv[])
 	if (context == NULL)
 		goto err1;
 	memset(context, 0, sizeof(struct physcontext));
+
+	context->width = width;
+	context->height = height;
 
 	context->tool = tool = nemotool_create();
 	if (tool == NULL)
@@ -140,9 +273,18 @@ int main(int argc, char *argv[])
 	nemoshow_canvas_set_dispatch_event(canvas, nemophys_dispatch_canvas_event);
 	nemoshow_one_attach(scene, canvas);
 
+	nemophys_prepare_bullet(context);
+
+	trans = nemoshow_transition_create(NEMOSHOW_LINEAR_EASE, 18000, 0);
+	nemoshow_transition_dirty_one(trans, context->canvas, NEMOSHOW_FILTER_DIRTY);
+	nemoshow_transition_set_repeat(trans, 0);
+	nemoshow_attach_transition(show, trans);
+
 	nemoshow_dispatch_frame(show);
 
 	nemotool_run(tool);
+
+	nemophys_finish_bullet(context);
 
 	nemoshow_destroy_view(show);
 
