620537d6edae7b45cbe478d17bd86552a0b894df
root <haruband@gmail.com>
2016-11-03 08:03:50 +0000

    db: Add insert/remove/query/modify operations

--- db/nemodb.c
+++ db/nemodb.c
@@ -13,6 +13,10 @@
 #include <nemodb.h>
 #include <nemomisc.h>
 
+struct dbiter {
+	mongoc_cursor_t *cursor;
+};
+
 struct nemodb {
 	mongoc_client_t *client;
 	mongoc_collection_t *collection;
@@ -64,6 +68,296 @@ int nemodb_use_collection(struct nemodb *db, const char *dbname, const char *dbc
 	db->collection = mongoc_client_get_collection(db->client, dbname, dbcollection);
 	if (db->collection == NULL)
 		return -1;
+
+	return 0;
+}
+
+static inline bson_t *nemodb_bson_from_ione(struct itemone *one)
+{
+	struct itemattr *attr;
+	bson_t *bson;
+
+	bson = bson_new();
+
+	nemoitem_attr_for_each(attr, one) {
+		BSON_APPEND_UTF8(bson,
+				nemoitem_attr_get_name(attr),
+				nemoitem_attr_get_value(attr));
+	}
+
+	return bson;
+}
+
+static inline struct itemone *nemodb_ione_from_bson(const bson_t *bson)
+{
+	struct itemone *one;
+	bson_iter_t iter;
+
+	one = nemoitem_one_create();
+
+	bson_iter_init(&iter, bson);
+
+	while (bson_iter_next(&iter)) {
+		nemoitem_one_set_attr(one,
+				bson_iter_key(&iter),
+				bson_iter_utf8(&iter, NULL));
+	}
+
+	return one;
+}
+
+int nemodb_insert_one(struct nemodb *db, const char *key, const char *value)
+{
+	bson_t *bson;
+	int r;
+
+	bson = bson_new();
+	BSON_APPEND_UTF8(bson, key, value);
+
+	r = mongoc_collection_insert(db->collection, MONGOC_INSERT_NONE, bson, NULL, NULL);
+
+	bson_destroy(bson);
+
+	return r;
+}
+
+int nemodb_insert_many(struct nemodb *db, struct itemone *one)
+{
+	bson_t *bson;
+	int r;
+
+	bson = nemodb_bson_from_ione(one);
+
+	r = mongoc_collection_insert(db->collection, MONGOC_INSERT_NONE, bson, NULL, NULL);
+
+	bson_destroy(bson);
+
+	return r;
+}
+
+int nemodb_remove_one(struct nemodb *db, const char *key, const char *value)
+{
+	bson_t *bson;
+	int r;
+
+	bson = bson_new();
+	BSON_APPEND_UTF8(bson, key, value);
+
+	r = mongoc_collection_remove(db->collection, MONGOC_REMOVE_NONE, bson, NULL, NULL);
+
+	bson_destroy(bson);
+
+	return r;
+}
+
+int nemodb_remove_many(struct nemodb *db, struct itemone *one)
+{
+	bson_t *bson;
+	int r;
+
+	bson = nemodb_bson_from_ione(one);
+
+	r = mongoc_collection_remove(db->collection, MONGOC_REMOVE_NONE, bson, NULL, NULL);
+
+	bson_destroy(bson);
+
+	return r;
+}
+
+char *nemodb_query_one_by_one(struct nemodb *db, const char *key, const char *value, const char *name)
+{
+	mongoc_cursor_t *cursor;
+	const bson_t *bdoc;
+	bson_error_t berr;
+	bson_t *bson;
+
+	bson = bson_new();
+	BSON_APPEND_UTF8(bson, key, value);
+
+	cursor = mongoc_collection_find(db->collection, MONGOC_QUERY_NONE, 0, 0, 0, bson, NULL, NULL);
+
+	bson_destroy(bson);
+
+	if (mongoc_cursor_next(cursor, &bdoc)) {
+		bson_iter_t iter;
+
+		bson_iter_init(&iter, bdoc);
+		if (bson_iter_find(&iter, name)) {
+			mongoc_cursor_destroy(cursor);
+
+			return strdup(bson_iter_utf8(&iter, NULL));
+		}
+	}
+
+	if (mongoc_cursor_error(cursor, &berr))
+		return NULL;
+
+	mongoc_cursor_destroy(cursor);
+
+	return NULL;
+}
+
+char *nemodb_query_one_by_many(struct nemodb *db, struct itemone *one, const char *name)
+{
+	mongoc_cursor_t *cursor;
+	const bson_t *bdoc;
+	bson_error_t berr;
+	bson_t *bson;
+
+	bson = nemodb_bson_from_ione(one);
+
+	cursor = mongoc_collection_find(db->collection, MONGOC_QUERY_NONE, 0, 0, 0, bson, NULL, NULL);
+
+	bson_destroy(bson);
+
+	if (mongoc_cursor_next(cursor, &bdoc)) {
+		bson_iter_t iter;
+
+		bson_iter_init(&iter, bdoc);
+		if (bson_iter_find(&iter, name)) {
+			mongoc_cursor_destroy(cursor);
+
+			return strdup(bson_iter_utf8(&iter, NULL));
+		}
+	}
+
+	if (mongoc_cursor_error(cursor, &berr))
+		return NULL;
+
+	mongoc_cursor_destroy(cursor);
+
+	return NULL;
+}
+
+struct itemone *nemodb_query_many_by_one(struct nemodb *db, const char *key, const char *value)
+{
+	mongoc_cursor_t *cursor;
+	const bson_t *bdoc;
+	bson_error_t berr;
+	bson_t *bson;
+
+	bson = bson_new();
+	BSON_APPEND_UTF8(bson, key, value);
+
+	cursor = mongoc_collection_find(db->collection, MONGOC_QUERY_NONE, 0, 0, 0, bson, NULL, NULL);
+
+	bson_destroy(bson);
+
+	if (mongoc_cursor_next(cursor, &bdoc)) {
+		mongoc_cursor_destroy(cursor);
+
+		return nemodb_ione_from_bson(bdoc);
+	}
+
+	if (mongoc_cursor_error(cursor, &berr))
+		return NULL;
+
+	mongoc_cursor_destroy(cursor);
+
+	return NULL;
+}
+
+struct itemone *nemodb_query_many_by_many(struct nemodb *db, struct itemone *one)
+{
+	mongoc_cursor_t *cursor;
+	const bson_t *bdoc;
+	bson_error_t berr;
+	bson_t *bson;
+
+	bson = nemodb_bson_from_ione(one);
+
+	cursor = mongoc_collection_find(db->collection, MONGOC_QUERY_NONE, 0, 0, 0, bson, NULL, NULL);
+
+	bson_destroy(bson);
+
+	if (mongoc_cursor_next(cursor, &bdoc)) {
+		mongoc_cursor_destroy(cursor);
+
+		return nemodb_ione_from_bson(bdoc);
+	}
+
+	if (mongoc_cursor_error(cursor, &berr))
+		return NULL;
+
+	mongoc_cursor_destroy(cursor);
+
+	return NULL;
+}
+
+struct dbiter *nemodb_query_iter_by_one(struct nemodb *db, const char *key, const char *value)
+{
+	struct dbiter *iter;
+	mongoc_cursor_t *cursor;
+	const bson_t *bdoc;
+	bson_error_t berr;
+	bson_t *bson;
+
+	bson = bson_new();
+	BSON_APPEND_UTF8(bson, key, value);
+
+	cursor = mongoc_collection_find(db->collection, MONGOC_QUERY_NONE, 0, 0, 0, bson, NULL, NULL);
+
+	bson_destroy(bson);
+
+	if (mongoc_cursor_error(cursor, &berr))
+		return NULL;
+
+	iter = (struct dbiter *)malloc(sizeof(struct dbiter));
+	iter->cursor = cursor;
+
+	return iter;
+}
+
+struct dbiter *nemodb_query_iter_by_many(struct nemodb *db, struct itemone *one)
+{
+	struct dbiter *iter;
+	mongoc_cursor_t *cursor;
+	const bson_t *bdoc;
+	bson_error_t berr;
+	bson_t *bson;
+
+	bson = nemodb_bson_from_ione(one);
+
+	cursor = mongoc_collection_find(db->collection, MONGOC_QUERY_NONE, 0, 0, 0, bson, NULL, NULL);
+
+	bson_destroy(bson);
+
+	if (mongoc_cursor_error(cursor, &berr))
+		return NULL;
+
+	iter = (struct dbiter *)malloc(sizeof(struct dbiter));
+	iter->cursor = cursor;
+
+	return iter;
+}
+
+struct itemone *nemodb_iter_next(struct dbiter *iter)
+{
+	const bson_t *bson;
+
+	if (mongoc_cursor_next(iter->cursor, &bson) == 0)
+		return NULL;
+
+	return nemodb_ione_from_bson(bson);
+}
+
+int nemodb_modify_many_to_many(struct nemodb *db, struct itemone *sone, struct itemone *done)
+{
+	bson_t *qson;
+	bson_t *bson;
+	bson_t *uson;
+
+	qson = nemodb_bson_from_ione(sone);
+	bson = nemodb_bson_from_ione(done);
+
+	uson = bson_new();
+	BSON_APPEND_DOCUMENT(uson, "$set", bson);
+
+	if (mongoc_collection_find_and_modify(db->collection, qson, NULL, uson, NULL, false, false, true, NULL, NULL) == 0)
+		return -1;
+
+	bson_destroy(uson);
+	bson_destroy(qson);
 
 	return 0;
 }
