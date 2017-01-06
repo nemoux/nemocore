9359fd6aaec33adde0a36fd143877c1ba5b9f86d
haruband <haruband@gmail.com>
2017-01-07 02:28:43 +0900

    cook: Fix scale/rotate pinning

--- cook/cooktrans.c
+++ cook/cooktrans.c
@@ -55,13 +55,13 @@ int nemocook_transform_update(struct cooktrans *trans)
 	nemomatrix_init_identity(&trans->matrix);
 
 	if (trans->sx != 1.0f || trans->sy != 1.0f || trans->sz != 1.0f) {
-		nemomatrix_translate_xyz(&trans->matrix, -trans->scale.px, -trans->scale.py, -trans->scale.pz);
+		nemomatrix_translate_xyz(&trans->matrix, trans->scale.px, trans->scale.py, trans->scale.pz);
 		nemomatrix_scale_xyz(&trans->matrix, trans->sx, trans->sy, trans->sz);
 		nemomatrix_translate_xyz(&trans->matrix, trans->scale.ux, trans->scale.uy, trans->scale.uz);
 	}
 
 	if (trans->rx != 0.0f || trans->ry != 0.0f || trans->rz != 0.0f) {
-		nemomatrix_translate_xyz(&trans->matrix, -trans->rotate.px, -trans->rotate.py, -trans->rotate.pz);
+		nemomatrix_translate_xyz(&trans->matrix, trans->rotate.px, trans->rotate.py, trans->rotate.pz);
 		nemomatrix_rotate_x(&trans->matrix, cos(trans->rx), sin(trans->rx));
 		nemomatrix_rotate_y(&trans->matrix, cos(trans->ry), sin(trans->ry));
 		nemomatrix_rotate_z(&trans->matrix, cos(trans->rz), sin(trans->rz));
