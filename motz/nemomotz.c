327c144b15ab743bc526affed25d81c5ac548e9a
haruband <haruband@gmail.com>
2017-01-31 17:03:56 +0900

    motz: Add motzone

--- motz/nemomotz.c
+++ motz/nemomotz.c
@@ -17,10 +17,67 @@ struct nemomotz *nemomotz_create(void)
 		return NULL;
 	memset(motz, 0, sizeof(struct nemomotz));
 
+	nemolist_init(&motz->one_list);
+
 	return motz;
 }
 
 void nemomotz_destroy(struct nemomotz *motz)
 {
+	nemolist_remove(&motz->one_list);
+
 	free(motz);
+}
+
+void nemomotz_attach_one(struct nemomotz *motz, struct motzone *one)
+{
+	nemolist_insert_tail(&motz->one_list, &one->link);
+}
+
+void nemomotz_detach_one(struct nemomotz *motz, struct motzone *one)
+{
+	nemolist_remove(&one->link);
+	nemolist_init(&one->link);
+}
+
+void nemomotz_one_draw_null(struct motzone *one)
+{
+}
+
+void nemomotz_one_down_null(struct motzone *one, float x, float y)
+{
+}
+
+void nemomotz_one_motion_null(struct motzone *one, float x, float y)
+{
+}
+
+void nemomotz_one_up_null(struct motzone *one, float x, float y)
+{
+}
+
+struct motzone *nemomotz_one_create(void)
+{
+	struct motzone *one;
+
+	one = (struct motzone *)malloc(sizeof(struct motzone));
+	if (one == NULL)
+		return NULL;
+	memset(one, 0, sizeof(struct motzone));
+
+	nemolist_init(&one->link);
+
+	one->draw = nemomotz_one_draw_null;
+	one->down = nemomotz_one_down_null;
+	one->motion = nemomotz_one_motion_null;
+	one->up = nemomotz_one_up_null;
+
+	return one;
+}
+
+void nemomotz_one_destroy(struct motzone *one)
+{
+	nemolist_remove(&one->link);
+
+	free(one);
 }
--- motz/nemomotz.h
+++ motz/nemomotz.h
@@ -9,11 +9,94 @@ NEMO_BEGIN_EXTERN_C
 
 #include <stdint.h>
 
+#include <nemolist.h>
+
+struct nemomotz;
+struct motzone;
+
+typedef void (*nemomotz_one_draw_t)(struct motzone *one);
+typedef void (*nemomotz_one_down_t)(struct motzone *one, float x, float y);
+typedef void (*nemomotz_one_motion_t)(struct motzone *one, float x, float y);
+typedef void (*nemomotz_one_up_t)(struct motzone *one, float x, float y);
+
 struct nemomotz {
+	struct nemolist one_list;
+};
+
+struct motzone {
+	nemomotz_one_draw_t draw;
+	nemomotz_one_down_t down;
+	nemomotz_one_motion_t motion;
+	nemomotz_one_up_t up;
+
+	struct nemolist link;
 };
 
 extern struct nemomotz *nemomotz_create(void);
 extern void nemomotz_destroy(struct nemomotz *motz);
+
+extern void nemomotz_attach_one(struct nemomotz *motz, struct motzone *one);
+extern void nemomotz_detach_one(struct nemomotz *motz, struct motzone *one);
+
+extern struct motzone *nemomotz_one_create(void);
+extern void nemomotz_one_destroy(struct motzone *one);
+
+extern void nemomotz_one_draw_null(struct motzone *one);
+extern void nemomotz_one_down_null(struct motzone *one, float x, float y);
+extern void nemomotz_one_motion_null(struct motzone *one, float x, float y);
+extern void nemomotz_one_up_null(struct motzone *one, float x, float y);
+
+static inline void nemomotz_one_set_draw_callback(struct motzone *one, nemomotz_one_draw_t draw)
+{
+	if (draw != NULL)
+		one->draw = draw;
+	else
+		one->draw = nemomotz_one_draw_null;
+}
+
+static inline void nemomotz_one_draw(struct motzone *one)
+{
+	one->draw(one);
+}
+
+static inline void nemomotz_one_set_down_callback(struct motzone *one, nemomotz_one_down_t down)
+{
+	if (down != NULL)
+		one->down = down;
+	else
+		one->down = nemomotz_one_down_null;
+}
+
+static inline void nemomotz_one_down(struct motzone *one, float x, float y)
+{
+	one->down(one, x, y);
+}
+
+static inline void nemomotz_one_set_motion_callback(struct motzone *one, nemomotz_one_motion_t motion)
+{
+	if (motion != NULL)
+		one->motion = motion;
+	else
+		one->motion = nemomotz_one_motion_null;
+}
+
+static inline void nemomotz_one_motion(struct motzone *one, float x, float y)
+{
+	one->motion(one, x, y);
+}
+
+static inline void nemomotz_one_set_up_callback(struct motzone *one, nemomotz_one_up_t up)
+{
+	if (up != NULL)
+		one->up = up;
+	else
+		one->up = nemomotz_one_up_null;
+}
+
+static inline void nemomotz_one_up(struct motzone *one, float x, float y)
+{
+	one->up(one, x, y);
+}
 
 #ifdef __cplusplus
 NEMO_END_EXTERN_C
