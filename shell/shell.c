00be9a078156324c0644c1d2cf42156fb59739f2
haruband <haruband@gmail.com>
2017-05-31 10:07:21 +0900

    shell: Fix fullscreen target checking mechanisms

--- shell/shell.c
+++ shell/shell.c
@@ -1503,7 +1503,7 @@ void nemoshell_kill_fullscreen_bin(struct nemoshell *shell, uint32_t target)
 	struct shellbin *sbin, *nbin;
 
 	wl_list_for_each(screen, &shell->fullscreen_list, link) {
-		if (target != 0 && screen->target != target)
+		if (screen->target & target == 0)
 			continue;
 
 		wl_list_for_each_safe(sbin, nbin, &screen->bin_list, screen_link) {
