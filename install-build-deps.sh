736110008e01e574daed6f70b7314f1e058fb892
haruband <haruband@gmail.com>
2018-01-09 05:04:12 -0500

    Add install-build-deps.sh to install development packages for building

---
+++ install-build-deps.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt-get -y install build-essential
+apt-get -y install libfreetype6-dev libpixman-1-dev libpng-dev libjpeg-dev libharfbuzz-dev libfontconfig1-dev
+apt-get -y install libevdev-dev libmtdev-dev libudev-dev
+apt-get -y install libgl1-mesa-dev libgles2-mesa-dev libegl1-mesa-dev libgbm-dev libdrm-dev
+apt-get -y install libbson-dev libjson-c-dev
+apt-get -y install liblo-dev
+apt-get -y install uuid-dev
+apt-get -y install libcurl4-openssl-dev
+apt-get -y install libmongoc-dev
+apt-get -y install librsvg2-dev
+apt-get -y install libavutil-dev libavcodec-dev libavformat-dev libswscale-dev
+apt-get -y install libao-dev
+apt-get -y install libpulse-dev libpulse-mainloop-glib
+
+apt-get -y install libwayland-client0 libwayland-cursor0 libwayland-server0 libwayland-dev libwayland-egl1-mesa
+
+apt-get -y install libxcb1-dev libxcb-composite0-dev libxcb-render0-dev libxcb-xfixes0-dev libxcb-cursor-dev
